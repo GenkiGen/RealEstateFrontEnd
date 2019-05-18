bf2393c44d3d85753412a1319b711cd2502a712f
JohnGenki <ryanz201278@yahoo.com.vn>
2019-05-18 14:35:35 +0700

    Updates

---
+++ precache-manifest.b345c697f2271c2ab54a2a6215d24185.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae3974c7362cf2f9a734",
+    "url": "/RealEstateFrontEnd/static/js/runtime~main.373471e6.js"
+  },
+  {
+    "revision": "e60828e8d6ad93c6afbf",
+    "url": "/RealEstateFrontEnd/static/js/main.4c2d9db0.chunk.js"
+  },
+  {
+    "revision": "e66a86b091ef6c428e9c",
+    "url": "/RealEstateFrontEnd/static/js/2.a3e67afa.chunk.js"
+  },
+  {
+    "revision": "f4e662de12804dbd111c8d05d328ef3d",
+    "url": "/RealEstateFrontEnd/index.html"
+  }
+];
